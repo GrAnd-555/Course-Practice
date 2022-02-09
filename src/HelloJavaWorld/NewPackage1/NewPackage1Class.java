00451be47b66b92153693be3e2475077006e5376
andriangratii <gr_and@yahoo.com>
2022-02-09 16:15:22 +0200

    Commiting HelloJavaWorld Packeage + 2 subPackages

---
+++ src/HelloJavaWorld/NewPackage1/NewPackage1Class.java
@@ -0,0 +1,8 @@
+package HelloJavaWorld.NewPackage1;
+
+public class NewPackage1Class {
+    public void newPrint1(){
+      System.out.println("NewPackage1Class");
+      System.out.println("NewPrint1");
+    }
+}
---
+++ src/HelloJavaWorld/NewPackage2/newPackage2Class.java
@@ -0,0 +1,8 @@
+package HelloJavaWorld.NewPackage2;
+
+public class newPackage2Class {
+    public void newPrint2(){
+        System.out.println("NewPackage2Class");
+        System.out.println("NewPrint2");
+    }
+}
